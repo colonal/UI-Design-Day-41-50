3f49383c7dab919322898f494254e5536dbb78cd
Mohammad Ebbeni <colonal.ch@gmail.com>
2022-07-07 15:50:58 +0300

    Update Home Screen

---
+++ lib/home/screens.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+
+import '../day41/day_41_screen.dart';
+import '../day42/day_42_screen.dart';
+import '../day43/day_43_screen.dart';
+import '../day44/day_44_screen.dart';
+import '../day45/day_45_screen.dart';
+import '../day46/day_46_screen.dart';
+import '../day47/day_47_screen.dart';
+import '../day48/day_48_screen.dart';
+import '../day49/day_49_screen.dart';
+import '../day50/day_50_screen.dart';
+
+const List screens = [
+  [
+    "Day 41",
+    "Apartment Booking App",
+    "screenshot/day41/1.jpg",
+    Day41Screen(),
+    Color(0xffEEEEEE),
+  ],
+  [
+    "Day 42",
+    "Travel App Design",
+    "screenshot/day42/1.jpg",
+    Day42Screen(),
+    Color(0xff616EBE),
+  ],
+  [
+    "Day 43",
+    "Caren Family Onboarding",
+    "screenshot/day43/1.jpg",
+    Day43Screen(),
+    Color(0xff01B06D),
+  ],
+  [
+    "Day 44",
+    "Travel Mobile App Template",
+    "screenshot/day44/1.png",
+    Day44Scree(),
+    Color(0xffFEFFFB),
+  ],
+  [
+    "Day 45",
+    "Plant Shop App",
+    "screenshot/day45/1.png",
+    Day45Screen(),
+    Color(0xffA5D6A7),
+  ],
+  [
+    "Day 46",
+    "Wallet App",
+    "screenshot/day46/1.png",
+    Day46Screen(),
+    Color(0xff5D5C7F),
+  ],
+  [
+    "Day 47",
+    "3D+ Art Marketplace",
+    "screenshot/day47/1.png",
+    Day47Screen(),
+    Color(0xff9646FE),
+  ],
+  [
+    "Day 48",
+    "Medicine Delivery",
+    "screenshot/day48/1.png",
+    Day48Screen(),
+    Color(0xffE8F4F2),
+  ],
+  [
+    "Day 49",
+    "Mobile Sign up/ Sign in",
+    "screenshot/day49/1.png",
+    Day49Screen(),
+    Color(0xff3960DC),
+  ],
+  [
+    "Day 50",
+    "Fitness App",
+    "screenshot/day50/1.png",
+    Day50Screen(),
+    Color(0xff000000),
+  ],
+];
--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:ui_design_day_41_50/day41/day_41_screen.dart';
-import 'package:ui_design_day_41_50/day42/day_42_screen.dart';
-import 'package:ui_design_day_41_50/day43/day_43_screen.dart';
-import 'package:ui_design_day_41_50/day44/day_44_screen.dart';
-import 'package:ui_design_day_41_50/day45/day_45_screen.dart';
-import 'package:ui_design_day_41_50/day46/day_46_screen.dart';
-import 'package:ui_design_day_41_50/day47/day_47_screen.dart';
-import 'package:ui_design_day_41_50/day48/day_48_screen.dart';
-import 'package:ui_design_day_41_50/day49/day_49_screen.dart';
 
 import 'app_scroll_behavior.dart';
-import 'day50/day_50_screen.dart';
+import 'home/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -19,184 +10,14 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'UI Examples',
       scrollBehavior: AppScrollBehavior(),
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        //
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            MaterialButton(
-              color: Colors.grey,
-              onPressed: () => Navigator.of(context)
-                  .push(MaterialPageRoute(builder: (_) => const Day41Screen())),
-              child: const Text(
-                "Day 41",
-                style: TextStyle(
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 20,
-                ),
-              ),
-            ),
-            const SizedBox(height: 20),
-            MaterialButton(
-              color: Colors.redAccent,
-              onPressed: () => Navigator.of(context)
-                  .push(MaterialPageRoute(builder: (_) => const Day42Screen())),
-              child: const Text(
-                "Day 42",
-                style: TextStyle(
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 20,
-                ),
-              ),
-            ),
-            const SizedBox(height: 20),
-            MaterialButton(
-              color: Colors.orangeAccent,
-              onPressed: () => Navigator.of(context)
-                  .push(MaterialPageRoute(builder: (_) => const Day43Screen())),
-              child: const Text(
-                "Day 43",
-                style: TextStyle(
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 20,
-                ),
-              ),
-            ),
-            const SizedBox(height: 20),
-            MaterialButton(
-              color: Colors.yellow[900],
-              onPressed: () => Navigator.of(context)
-                  .push(MaterialPageRoute(builder: (_) => const Day44Scree())),
-              child: const Text(
-                "Day 44",
-                style: TextStyle(
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 20,
-                ),
-              ),
-            ),
-            const SizedBox(height: 20),
-            MaterialButton(
-              color: Colors.grey[900],
-              onPressed: () => Navigator.of(context)
-                  .push(MaterialPageRoute(builder: (_) => const Day45Screen())),
-              child: const Text(
-                "Day 45",
-                style: TextStyle(
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 20,
-                ),
-              ),
-            ),
-            const SizedBox(height: 20),
-            MaterialButton(
-              color: const Color(0xff3C3B80),
-              onPressed: () => Navigator.of(context)
-                  .push(MaterialPageRoute(builder: (_) => const Day46Screen())),
-              child: const Text(
-                "Day 46",
-                style: TextStyle(
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 20,
-                ),
-              ),
-            ),
-            const SizedBox(height: 20),
-            MaterialButton(
-              color: const Color(0xff3C3B80),
-              onPressed: () => Navigator.of(context)
-                  .push(MaterialPageRoute(builder: (_) => const Day47Screen())),
-              child: const Text(
-                "Day 47",
-                style: TextStyle(
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 20,
-                ),
-              ),
-            ),
-            const SizedBox(height: 20),
-            MaterialButton(
-              color: const Color.fromARGB(255, 59, 128, 128),
-              onPressed: () => Navigator.of(context)
-                  .push(MaterialPageRoute(builder: (_) => const Day48Screen())),
-              child: const Text(
-                "Day 48",
-                style: TextStyle(
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 20,
-                ),
-              ),
-            ),
-            const SizedBox(height: 20),
-            MaterialButton(
-              color: const Color.fromARGB(255, 128, 59, 113),
-              onPressed: () => Navigator.of(context)
-                  .push(MaterialPageRoute(builder: (_) => const Day49Screen())),
-              child: const Text(
-                "Day 49",
-                style: TextStyle(
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 20,
-                ),
-              ),
-            ),
-            const SizedBox(height: 20),
-            MaterialButton(
-              color: const Color.fromARGB(255, 124, 210, 126),
-              onPressed: () => Navigator.of(context)
-                  .push(MaterialPageRoute(builder: (_) => const Day50Screen())),
-              child: const Text(
-                "Day 50",
-                style: TextStyle(
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 20,
-                ),
-              ),
-            ),
-          ],
-        ),
-      ),
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const HomeScreen(),
     );
   }
 }
